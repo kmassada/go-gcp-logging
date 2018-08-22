3b1a3f606537826cd70bf5e168dea0f748545954
Kenneth Massada <kmassada@gmail.com>
2018-08-21 17:08:05 -0700

    main.go

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"cloud.google.com/go/logging"
+)
+
+type Response struct {
+	Ip      string
+	Headers http.Header
+}
+
+func printHeader(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	response := Response{r.RemoteAddr, r.Header}
+	json.NewEncoder(w).Encode(response)
+	lg.Log(logging.Entry{Payload: response})
+
+}
+func main() {
+
+	// Initialize a logger
+	lg := client.Logger("my-log")
+
+	// Add entry to log buffer
+	lg.Log(logging.Entry{Payload: "something happened!"})
+
+	// HTTP handler
+	http.HandleFunc("/", printHeader)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
