0e4ba058ac7afbe8fec8e4185dbf5f5e6c7a364d
Kenneth Massada <kmassada@gmail.com>
2018-08-26 00:48:27 -0700

    lint and formatting

--- main.go
+++ main.go
@@ -1,23 +1,26 @@
 package main
 
 import (
-	"encoding/json"
-	"net/http"
-	"cloud.google.com/go/logging"
-	"google.golang.org/api/option"
 	"context"
-	"os"
+	"encoding/json"
 	"fmt"
 	stdlog "log"
+	"net/http"
+	"os"
+
+	"cloud.google.com/go/logging"
+	"google.golang.org/api/option"
 )
 
+// Response represents a response to http.
 type Response struct {
-	Ip      string
+	IP      string
 	Headers http.Header
 }
 
+// Server to handle logging
 type Server struct {
-	lg	 *logging.Logger
+	lg *logging.Logger
 }
 
 func (s *Server) printHeader(w http.ResponseWriter, r *http.Request) {
@@ -27,16 +30,16 @@ func (s *Server) printHeader(w http.ResponseWriter, r *http.Request) {
 	response := Response{r.RemoteAddr, r.Header}
 	json.NewEncoder(w).Encode(response)
 	s.lg.Log(logging.Entry{
-		Payload: response,
+		Payload:  response,
 		Severity: logging.Info,
 	})
 
 }
 func main() {
-	
+
 	// context
 	ctx := context.Background()
-	client, err := logging.NewClient(ctx, 
+	client, err := logging.NewClient(ctx,
 		os.Getenv("PROJECT_ID"),
 		option.WithCredentialsFile(os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")))
 	if err != nil {
@@ -49,7 +52,7 @@ func main() {
 	s := Server{lg}
 
 	// Add entry to log buffer
-	j := []byte(`{"Data": {"Hostname": "`+os.Getenv("HOSTNAME")+`", "Count": 3}}`)
+	j := []byte(`{"Data": {"Hostname": "` + os.Getenv("HOSTNAME") + `", "Count": 3}}`)
 
 	message := fmt.Sprintf(`%s`, json.RawMessage(j))
 	stdlog.Output(0, message)
@@ -57,15 +60,14 @@ func main() {
 	fmt.Println(message)
 
 	lg.Log(logging.Entry{
-		Payload: message,
+		Payload:  message,
 		Severity: logging.Critical,
 	})
 
 	lg.Log(logging.Entry{
-		Payload: json.RawMessage(j),
+		Payload:  json.RawMessage(j),
 		Severity: logging.Critical,
 	})
-
 
 	// HTTP handler
 	http.HandleFunc("/", s.printHeader)
